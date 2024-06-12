27d58ec8416cd5c54d05a6d02e511d0adfd93098
wyeeeee <1563031828@qq.com>
2024-06-12 17:19:19 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process;
+mod utils;
+use utils::config::run;
+use utils::parse::parse;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config =  parse(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
